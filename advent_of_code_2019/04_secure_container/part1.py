9bc45ba6cbfd98a57b5c2b4473996c5803363f9d
Loic Teixeira <hello@lta.me>
2019-12-04 06:52:24 +0100

    Add challenge AoC19.4.1

---
+++ advent_of_code_2019/04_secure_container/part1.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Iterable
+
+
+def is_valid_password(password: int) -> bool:
+    digits = map(int, str(password))
+
+    previous_digit = 0
+    has_equal_adjacent_digits = False
+    for digit in digits:
+        # Equal adjacent digits
+        if digit == previous_digit:
+            has_equal_adjacent_digits = True
+
+        # Never decrease
+        if digit < previous_digit:
+            return False
+        previous_digit = digit
+
+    return has_equal_adjacent_digits
+
+
+def valid_passwords_in_range(inputs: str) -> Iterable[int]:
+    lower_bound, upper_bound = map(int, inputs.split("-"))
+    password_candidates = range(lower_bound, upper_bound + 1)
+    return filter(is_valid_password, password_candidates)
+
+
+if __name__ == "__main__":
+    input_file_path = Path(__file__).parent / "input.txt"
+    with open(input_file_path, "r") as fh:
+        data = fh.readlines()
+
+    valid_password = list(valid_passwords_in_range(data[0]))
+    print(f"There are `{len(valid_password)}` valid password within the given range")
