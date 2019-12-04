71801e75522947dbf89f314535a814d4e4ca1a93
Loic Teixeira <hello@lta.me>
2019-12-04 07:05:54 +0100

    Add challenge AoC19.4.2

--- advent_of_code_2019/04_secure_container/part2.py
+++ advent_of_code_2019/04_secure_container/part2.py
@@ -1,31 +1,26 @@
+from collections import defaultdict
 from pathlib import Path
-from typing import Callable, Iterable
+
+from part1 import valid_passwords_in_range
 
 
 def is_valid_password(password: int) -> bool:
     digits = map(int, str(password))
 
     previous_digit = 0
-    has_equal_adjacent_digits = False
+    equal_adjacent_digits = defaultdict(lambda: 1)
     for digit in digits:
         # Equal adjacent digits
         if digit == previous_digit:
-            has_equal_adjacent_digits = True
+            equal_adjacent_digits[digit] += 1
 
         # Never decrease
         if digit < previous_digit:
             return False
         previous_digit = digit
 
-    return has_equal_adjacent_digits
-
-
-def valid_passwords_in_range(
-    inputs: str, validation: Callable = is_valid_password
-) -> Iterable[int]:
-    lower_bound, upper_bound = map(int, inputs.split("-"))
-    password_candidates = range(lower_bound, upper_bound + 1)
-    return filter(validation, password_candidates)
+    doubles = [digit for digit, count in equal_adjacent_digits.items() if count == 2]
+    return len(doubles) > 0
 
 
 if __name__ == "__main__":
@@ -33,5 +28,5 @@ if __name__ == "__main__":
     with open(input_file_path, "r") as fh:
         data = fh.readlines()
 
-    valid_password = list(valid_passwords_in_range(data[0]))
+    valid_password = list(valid_passwords_in_range(data[0], is_valid_password))
     print(f"There are `{len(valid_password)}` valid password within the given range")
